0ee9185b6a41363f66693b2044842d3e3e4c85cf
FAS2024 <fasredtech@gmail.com>
2025-05-08 18:36:49 +0100

    I just completed the login page . the login page is working now

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path('', home, name = "home"),
+    path('', views.home, name = "home"),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_login, name='logout'),
+    # path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/student/', views.student_dashboard, name='student_dashboard'),
+    path('dashboard/parent/', views.parent_dashboard, name='parent_dashboard'),
+    path('dashboard/staff/', views.staff_dashboard, name='staff_dashboard'),
+    path('dashboard/branch_admin/', views.branch_admin_dashboard, name='branch_admin_dashboard'),
+    path('dashboard/superadmin/', views.superadmin_dashboard, name='superadmin_dashboard'),
 ]
+
