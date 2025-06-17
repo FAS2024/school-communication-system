30922c3e8daf9facd5b50ed5458450a22d173c56
FAS2024 <fasredtech@gmail.com>
2025-06-17 17:15:39 +0100

    I just updated the schedule time to have a/pm format

--- lagooz_coms/settings.py
+++ lagooz_coms/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -181,9 +182,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'  # This assumes the 'login' URL name exists
 
-
-
-from celery.schedules import crontab
 
 CELERY_BEAT_SCHEDULE = {
     'send-due-communications-every-minute': {
