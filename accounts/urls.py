cb23af1d4ecaac5a32963a938596f32157e07dbc
Fatai Sunmonu <fasredtech@gmail.com>
2025-08-10 08:14:14 +0100

    I unified the send and the edit communication in one class

--- accounts/urls.py
+++ accounts/urls.py
@@ -114,5 +114,9 @@ urlpatterns = [
     path('communications/drafts/<int:pk>/edit/', views.EditDraftMessageView.as_view(), name='edit_draft_message'),
     path('communications/drafts/<int:pk>/delete/', views.delete_draft_message, name='delete_draft_message'),
     path('communications/drafts/delete_all/', views.DeleteAllDraftMessagesView.as_view(), name='delete_all_draft_messages'),
+
+    # ////////////////////////////////////
+    path('communications/create/', views.CommunicationCreateEditView.as_view(), name='communication_create'),
+    path('communications/<int:pk>/edit/', views.CommunicationCreateEditView.as_view(), name='communication_edit_draft'),
 ]
 
